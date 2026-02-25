a45c100b5d63785e21765631c2ae1716a5954454
Jenny <156132099+Jennyznz@users.noreply.github.com>
2026-02-24 21:38:36 -0500

    Adjust module name and print message

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module https://github.com/Jennyznz/pokedex
+module github.com/Jennyznz/pokedex
 
 go 1.25.0
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import {
-	"fmt"
-}
+import "fmt"
 
 func main() {
-	fmt.Printlln("Hello, world!")
+	fmt.Println("Hello, World!")
 }
