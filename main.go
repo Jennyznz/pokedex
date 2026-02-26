428c2d4f3ad7ec86589d01fd206b4e9995f88340
Jenny <156132099+Jennyznz@users.noreply.github.com>
2026-02-25 23:17:00 -0500

    Add command support structure

--- main.go
+++ main.go
@@ -6,17 +6,57 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+var commands = map[string]cliCommand {
+	"exit": {
+		name: "exit",
+		description: "Exit the Pokedex",
+		callback: commandExit,
+	},
+	"help": {
+		name: "help",
+		description: "Displays available commands",
+		callback: commandHelp,
+	},
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	input := ""
 	cleaned := []string{}
+	command := ""
 
 	for {
 		fmt.Printf("Pokedex > ")
 		if scanner.Scan() == true {
 			input = scanner.Text()
 			cleaned = cleanInput(input)
-			fmt.Println("Your command was:", cleaned[0])
+			command = cleaned[0]
+			verifiedCommand, exists := commands[command]
+			if exists == true {
+				verifiedCommand.callback()
+			} else {
+				fmt.Println("Unknown command")
+			}
 		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage: \n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
 }
